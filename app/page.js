018f0e74401d765dbd3d998fa5dea4bf831f6de6
Naveen gupta <gjagarnath54@gmail.com>
2026-02-11 22:04:50 +0530

    first

--- app/page.js
+++ app/page.js
@@ -4,6 +4,24 @@ const AI = () => {
   return (
     <div>
       <Assistant/>
+
+
+    <button
+onClick={() => window.location.href="upi://pay?pa=ng968633@okicici&pn=Naveen&am=10&cu=INR"}
+style={{
+backgroundColor:"#0f9d58",
+color:"#fff",
+padding:"14px 28px",
+fontSize:"18px",
+border:"none",
+borderRadius:"8px",
+cursor:"pointer"
+}}
+
+>
+
+Pay ₹10 with Google Pay </button>
+
     </div>
   )
 }
